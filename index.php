f58b6072400bcea6d236bca7598d36af7f06b8e7
NOHELIA YANIBELLI <nyanibelli89@gmail.com>
2021-11-06 16:37:48 -0300

    ultimaversion

--- index.php
+++ index.php
@@ -7,39 +7,21 @@ $link=Conectarse();
 if ($activo = 1) {
     $activo = 1;
 
-    $resultado=mysqli_query($link,"select * from producto ");
+    $resultado=mysqli_query($link,"select * from producto where activo=1");
 while($filas = mysqli_fetch_array($resultado)){
-    
-$id=$filas["id"];
+   
+$id=$filas["id_producto"];
 $n=$filas["nombre"];
-$a=$filas["apellido"];
-$e=$filas["edad"];
+$a=$filas["descripcion"];
+$e=$filas["precio"];
+$e=$filas["stock"];
+$e=$filas["activo"];
+$e=$filas["imagen1"];
+$e=$filas["imagen2"];
+$e=$filas["imagen3"];
 echo $id." ".$n." ".$a." ".$e."<br>";
 }
 mysqli_close($link);
 ?>
-}
-
-$id=$_POST['txtId'];
-$nom=$_POST['txtNom'];
-$descripcion=$_POST['txtDescripcion'];
-$precio=$_POST['txtPrecio'];
-$stock=$_POST['txtStock'];
-echo $stock;
-$img1=$_POST['txtImagen1'];
-$img2=$_POST['txtImagen2'];
-$img3=$_POST['txtImagen3'];
-
-if ($stock > 0) {
-    $activo = 1;
-}
 
 
-
-mysqli_query($link,"insert into producto(id_producto, nombre, descripcion, precio, stock, activo,imagen1,imagen2,imagen3) VALUES ($id,'$nom','$descripcion',$precio,$stock,$activo,'$img1','$img2','$img3') ");
-
-
-
-
-mysqli_close($link);
-?>
